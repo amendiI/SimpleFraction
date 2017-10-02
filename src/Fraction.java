42ef38f953f178d529b8b45667a96221e9f57ffc
amendiI <raphaelmuller@hotmail.fr>
2017-10-02 14:46:23 +0200

    ajout to string

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,17 @@
 
 public class Fraction {
+	private int numerateur;
+	private int denominateur;
+	
+	public Fraction(int n,int d)
+	{
+		numerateur=n;
+		denominateur=d;
+	}
+	
+	public String toString()
+	{
+		return Integer.toString(numerateur)+'/'+Integer.toString(denominateur);
+	}
 
 }
--- src/main.java
+++ src/main.java
@@ -3,6 +3,6 @@ public class main {
 
 	public static void main(String[] args)
 	{
-		System.out.println("papa");;
+		System.out.println(new Fraction(4,5).toString());
 	}
 }
